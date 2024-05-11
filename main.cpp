e83fbd05233cd800d6bfc910c58ffc260c8bec3b
KrasikovViktor <vitya22898@gmail.com>
2024-05-11 18:06:02 +0300

    Moved get_AST.h/cpp to folder, edited main due this

--- main.cpp
+++ main.cpp
@@ -2,26 +2,27 @@
 #include <wchar.h>
 #include <string>
 #include <vector>
-#include "tree_creation.h"
 #include "get_AST.h"
+#include "Scanner.h"
+#include "Parser.h"
 
 main()
 {
-        try
-        {
-                wchar_t string[20] = L"MARKOV.IN";
-                wchar_t *file  = string;
-                ParseTree::AST* ast = new ParseTree::AST;
+    try
+    {
+        wchar_t string[200] = L"C:/Users/kapji/DocumentsW/projects/Markov_algorithm_compiler/MARKOV.IN";
+        wchar_t *file = string;
+        ParseTree::AST *ast = new ParseTree::AST;
 
-                Scanner *scanner = new Scanner(file);
-                Parser *parser   = new Parser(scanner, ast);
-                parser->Parse();
+        Scanner *scanner = new Scanner(file);
+        Parser *parser = new Parser(scanner, ast);
+        parser->Parse();
 
-                delete parser;
-                delete scanner;
-        }
-        catch (const std::runtime_error &e)
-        {
-                std::cerr << "Error: " << e.what() << std::endl;
-        }
+        delete parser;
+        delete scanner;
+    }
+    catch (const std::runtime_error &e)
+    {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
 }

85fe78f031f6c344416a634aa1168e81ad454018
KrasikovViktor <vitya22898@gmail.com>
2024-05-11 15:49:23 +0300

    Deleted unnecessary files

--- чтобы не потерялось/run_calc.bat
+++
@@ -1,9 +0,0 @@
-.\Coco.exe .\calc.cpp.atg
-g++ -c Parser.cpp -o parser.o 
-g++ -c Scanner.cpp -o scanner.o
-g++ -c main.cpp
-g++ -o tr.exe scanner.o parser.o main.o
-.\tr.exe .\calc.IN
-
-del Scanner.cpp.old Scanner.h.old Parser.cpp.old Parser.h.old 
-del scanner.o parser.o main.o

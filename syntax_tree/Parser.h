01717b81e8e65748a9aac90f75be3eaae62715d5
XehiDat <63257446+ZetroRetro@users.noreply.github.com>
2024-05-15 12:45:53 +0300

    WTF

---
+++ syntax_tree/Parser.h
@@ -0,0 +1,93 @@
+
+
+#if !defined(COCO_PARSER_H__)
+#define COCO_PARSER_H__
+
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include "AST.h"
+
+
+#include "Scanner.h"
+#include <locale>
+#include <codecvt>
+#include <string>
+
+
+
+class Errors {
+public:
+	int count;			// number of errors detected
+
+	Errors();
+	void SynErr(int line, int col, int n);
+	void Error(int line, int col, const wchar_t *s);
+	void Warning(int line, int col, const wchar_t *s);
+	void Warning(const wchar_t *s);
+	void Exception(const wchar_t *s);
+
+}; // Errors
+
+class Parser {
+private:
+	enum {
+		_EOF=0,
+		_ident=1,
+		_string=2
+	};
+	int maxT;
+
+	Token *dummyToken;
+	int errDist;
+	int minErrDist;
+	ParseTree::AST* root;
+
+	void SynErr(int n);
+	void Get();
+	void Expect(int n);
+	bool StartOf(int s);
+	void ExpectWeak(int n, int follow);
+	bool WeakSeparator(int n, int syFol, int repFol);
+
+public:
+	Scanner *scanner;
+	Errors  *errors;
+
+	Token *t;			// last recognized token
+	Token *la;			// lookahead token
+
+
+
+    ParseTree::AST* GetASTRoot();
+	Parser(Scanner *scanner, ParseTree::AST* a);
+	~Parser();
+	void SemErr(const wchar_t* msg);
+
+	void Root();
+	void Block(ParseTree::Block& B);
+	void Stat(ParseTree::Stat*& s, ParseTree::Block& B);
+	void Term(ParseTree::BinExpr*& b);
+	void Condition(ParseTree::Expr*& Cond);
+	void Link(ParseTree::Link& link);
+	void Goto(ParseTree::Goto& Gt);
+	void expression(ParseTree::Expr*& EXPR);
+	void TermOfIf(ParseTree::Expr*& TR);
+	void Secondary_expression(ParseTree::Expr*& SE);
+	void Primary_expression(ParseTree::Expr*& PE);
+	void Unury(ParseTree::Expr*& SE);
+	void Word(std::wstring &str);
+	void Ident(std::wstring &str);
+	void GOTO();
+	void SubOp();
+
+	void Parse();
+
+}; // end Parser
+
+
+
+#endif
+
